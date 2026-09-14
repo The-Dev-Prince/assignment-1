5e1c4d4dc7b38d540b3d1572de88b63f34159304
Hyrum Rohrbach <hyrum.rohrbach@gmail.com>
2026-09-14 17:07:27 -0600

    up

--- Python/main.py
+++ Python/main.py
@@ -28,13 +28,18 @@ with open(file_path, mode="r", encoding="utf-8") as f:
         print(f"Error: {file_path} has an incorrect or missing header.")
         sys.exit(1)
 
+    valid_records = []
+    invalid_records = []
     for row in lines:
         if len(row) != 5:
             print(f"Error: {file_path} has a row with an incorrect number of columns: {row}")
             continue  # Skip to the next row instead of crashing
         
-        #trow = [col.strip() for col in row]
-        validation(row)
+        trow = [col.strip() for col in row]
+        validation(trow)
+
+    if len(valid_records) == 0 and len(invalid_records) == 0:
+        print("File has a header but no data rows.")
 
 
 def validation(trow):
